c174ed7123084472829160a95e73dc15593b92d0
Malinna Leach <malinnab@yahoo.com>
2016-10-31 15:44:56 +0000

    starting web page structure

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class Makersbnb < Sinatra::Base
   get '/' do
-    'Hello Makersbnb!'
+    erb :index
   end
 
   # start the server if ruby file executed directly
---
+++ src/interface.js
@@ -0,0 +1,14 @@
+$( document ).ready(function () {
+
+  $("#1").text();
+  $("#2").text();
+  $("#3").text();
+  $("#4").text();
+  $("#5").text();
+
+
+
+
+
+
+});
