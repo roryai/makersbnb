fe37d73e725f64e1cdf0852bc4dff1a8cddeafac
Anna Lucking <annavlucking@hotmail.co.uk>
2016-11-01 11:16:19 +0000

    add log in and sign up views and html

--- app.rb
+++ app.rb
@@ -5,6 +5,14 @@ class Makersbnb < Sinatra::Base
     erb :index
   end
 
+  get '/sign-up' do
+    erb :sign_up
+  end
+
+  get '/log-in' do
+    erb :log_in
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
