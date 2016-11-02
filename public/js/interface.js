c095174f04109dacfa9a51b3100ec44e67c8d112
Rory Aitchison <rory.aitchison@gmail.com>
2016-11-02 12:22:50 +0000

    form data on submit page is stored in a variable via jquery code

--- public/js/interface.js
+++ public/js/interface.js
@@ -4,12 +4,23 @@ $( document ).ready(function () {
 
 
   $('#addSpace').click(function(){
-    var name = $('#name').val();
+    var name = $('#spaceName').val();
     var address = $('#address').val();
     var description = $('#description').val();
     var price = $('#price').val();
     var space = new Space(name, address, description, price, "Judy");
     console.log(space);
+  });
+
+  $('#signUp').click(function(){
+    var fullName = $('#fullName').val();
+    var userName = $('#userName').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var user = new User(fullName, userName, email, password);
+    window.location.href = "http://localhost:4567";
+    console.log(user);
+
   });
 
 });
