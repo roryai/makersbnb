e4eac7c95a29e704c73653800e94b9c162e65dbf
Rory Aitchison <rory.aitchison@gmail.com>
2016-11-02 15:04:00 +0000

    switching to Kath

--- public/js/interface.js
+++ public/js/interface.js
@@ -2,16 +2,6 @@
 
 $( document ).ready(function () {
 
-
-  $('#addSpace').click(function(){
-    var name = $('#spaceName').val();
-    var address = $('#address').val();
-    var description = $('#description').val();
-    var price = $('#price').val();
-    var space = new Space(name, address, description, price, "Judy");
-    console.log(space);
-  });
-
   $('#signUp').click(function(){
     var fullName = $('#fullName').val();
     var userName = $('#userName').val();
@@ -28,20 +18,29 @@ $( document ).ready(function () {
     var loginUserName = $('#loginUserName').val();
     var  loginPassword = $('#loginPassword').val();
 
-
-
         var username = "rory";
         var password = "kath";
 
         if ((loginUserName == username) && (loginPassword == password)) {
           alert ("success")
+          window.location.href = "http://localhost:4567/add-space";
             return true;
         }
         else {
             alert ("Login was unsuccessful, please check your username and password");
             return false;
         }
+  });
 
+  $('#addSpace').click(function(){
+    // var userName = *username from logged in user*
+    var name = $('#spaceName').val();
+    var address = $('#address').val();
+    var description = $('#description').val();
+    var price = $('#price').val();
+    var space = new Space(name, address, description, price, "user name here");
+    // user101.addSpace(space)
+    console.log(space);
   });
 
 });
