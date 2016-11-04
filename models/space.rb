c221e0a26b39dc812e317a2c1451f8673976c8a0
Malinna Leach <malinnab@yahoo.com>
2016-11-04 14:35:34 +0000

    can save and view space details

--- models/space.rb
+++ models/space.rb
@@ -4,13 +4,13 @@ class Space
   include DataMapper::Resource
 
   has n, :bookings, :through => Resource
+  belongs_to :user
 
   property :id, Serial
   property :space_name, String
   property :address, String
   property :desc, Text
   property :price, Integer
-  property :owner, String
 
 
 end
